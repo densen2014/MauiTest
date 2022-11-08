fe28bfb7b65083d10fe3cb220b254b62d5efbc8d
Alex chow <zhouchuanglin@gmail.com>
2022-11-08 12:51:49 +0100

    添加项目文件。

---
+++ MauiBlazor1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiBlazor1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
